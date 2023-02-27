61e48bb4dd6e7834a817eaab1a6a028d4a12a8d8
Ewerton Cunha <ewertoncunha@hotmail.com>
2023-02-27 20:17:49 -0300

    6 exercicios do dia feitos

---
+++ exercicio.js
@@ -0,0 +1,79 @@
+let a = 100;
+let b = 90;
+let c = -10;
+
+//console.log(a + b);
+//console.log(a - b);
+//console.log(a * b);
+//console.log(a / b);
+//console.log(a % b);
+
+// if (a > b) {
+//     console.log(a);
+// } else {
+//     console.log(b);
+// }
+
+// if (a > b && a > c) {
+//     console.log(a);
+// } else if (b > a && b > c) {
+//     console.log(b);
+// } else {
+//     console.log(c);
+// }
+
+// if (a > 0) {
+//     console.log('positivo');
+// } else if (a == 0) {
+//     console.log('zero');
+// } else {
+//     console.log('negativo');
+// }
+
+// if (a+b+c==180 && a > 0 && b > 0 && c >0) {
+//     console.log('true');
+// } else if (a+b+c!=180) {
+//     console.log('false');
+// } else{
+//     console.log('angulo negativo aqui nao irmao')
+// }
+
+// let xadrez = 'knight'.toLowerCase();
+// switch (xadrez) {
+//     case 'pawn':
+//         console.log('forward');
+//         break;
+//     case 'bishop':
+//         console.log('diagonals');
+//         break;
+//     case 'knight':
+//         console.log('L');
+//         break;
+//     case 'rook':
+//         console.log('horizontals, verticals');
+//         break;
+//     case 'queen':
+//         console.log('all in one direction');
+//         break;
+//     case 'king':
+//         console.log('one square in any direction');
+//         break;
+//     default:
+//         break;
+// }
+
+let nota = (7.5)*10;
+
+if (nota < 0 | nota > 100) {
+    console.log('erro');
+}else if (nota>= 90) {
+    console.log('A');
+}else if (nota>= 80) {
+    console.log('B');
+}else if (nota>= 70) {
+    console.log('C');
+}else if (nota >= 60) {
+    console.log('D');
+}else if (nota < 50) {
+    console.log('F');
+}
---
+++ scripts.js
@@ -0,0 +1,11 @@
+let age = 20;
+let name = 'Hulk';
+let myName = 'Ewerton';
+let birthCity = 'Belem';
+let birthYear = '1996';
+
+console.log(age);
+console.log(name);
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
