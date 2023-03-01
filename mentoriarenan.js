9ba4ebc8c04fc17fcd3fe24fbedf7655e89fa39c
Ewerton Cunha <ewertoncunha@hotmail.com>
2023-03-01 18:03:38 -0300

    exercicio do renan 1 e 2 feitos

---
+++ mentoriarenan.js
@@ -0,0 +1,24 @@
+// let numbers = [2,6,18,33,90];
+// let result = 6;
+
+// for (let index = 0; index < numbers.length; index+=1) {
+//     if (numbers[index] == result){
+//         console.log('o numero',numbers[index],'foi encontrado na posicao', index);
+//     } else{
+//         console.log('Elemento não encontrado no array')
+//     }
+    
+// } 
+// let numbersImpares = [];
+
+
+// for (let index = 0; index < 50; index++) {
+//     if(index % 2 === 0) {
+        
+//     } else{
+//         numbersImpares.push(index)  
+//     }    
+// }
+
+// console.log(numbersImpares)
+
